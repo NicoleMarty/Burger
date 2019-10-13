f2edcb30e5c72746d04d92aa7fd987eafd2a6b4b
Nicole Marty <nmarty310@gmail.com>
2019-10-13 07:28:27 -0500

    connection working. getting error that burger.selectAll is not a function in burgers_controller.js

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burger", function(res) {
+            cb(res);
+        });
+    }
+};
