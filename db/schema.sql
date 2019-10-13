af01968fdda0a48ff4a9778207fa87a7d14c0013
Nicole Marty <nmarty310@gmail.com>
2019-10-13 06:48:22 -0500

    created database

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burger (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(50) NOT NULL,
+devoured BOOLEAN
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burger (burger_name) VALUES ("Bob's Burger");
+INSERT INTO burger (burger_name) VALUES ("Sweet Home Avodcado");
+INSERT INTO burger (burger_name) VALUES ("Poblano Picasso");
+INSERT INTO burger (burger_name) VALUES ("Eggers Can't Be Cheesers");
+INSERT INTO burger (burger_name) VALUES ("World's Okay-ist Burger");
+
+SELECT * FROM burger;
