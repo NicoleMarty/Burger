5829cd014131c5c75f908293368e7a08ddf98a71
Nicole Marty <nmarty310@gmail.com>
2019-10-14 12:34:24 -0500

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,8 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burger (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 burger_name VARCHAR(50) NOT NULL,
-devoured BOOLEAN
+devoured BOOLEAN DEFAULT FALSE
 );
